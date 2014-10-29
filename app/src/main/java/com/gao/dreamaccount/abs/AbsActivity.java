81974294cb0da22183fbf484e41eaa9aad7d4cb4
gaoqian526 <gq-526@163.com>
2014-10-29 15:12:01 +0800

    增加依赖注入

--- app/src/main/java/com/gao/dreamaccount/abs/AbsActivity.java
+++ app/src/main/java/com/gao/dreamaccount/abs/AbsActivity.java
@@ -1,7 +1,15 @@
 package com.gao.dreamaccount.abs;
 import android.support.v7.app.*;
+import android.os.*;
 
 public class AbsActivity extends ActionBarActivity
 {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
 	
 }
